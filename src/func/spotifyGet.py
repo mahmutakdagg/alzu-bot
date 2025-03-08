26508e7e3447ff883b6a90d90ceae0393b69341e
Alzurasep <alzurasep@gmail.com>
2025-03-08 21:41:32 +0300

    Update spotifyGet.py

--- src/func/spotifyGet.py
+++ src/func/spotifyGet.py
@@ -1,12 +1,12 @@
 import requests
 from dotenv import load_dotenv
-from os import getenv
+import os
 
 
 
 def getToken():
-    SpotifyClientID = getenv( 'SpotifyClientID' )
-    SpotifyClientSECRET = getenv( 'SpotifyClientSECRET' )
+    SpotifyClientID = os.environ( 'SpotifyClientID' )
+    SpotifyClientSECRET =  os.environ( 'SpotifyClientSECRET' )
 
     head = { 'Content-Type': "application/x-www-form-urlencoded" }
 
