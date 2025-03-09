07ff5c3f55e51e581ed19e61d1c98a7557c66270
mahmutakdagg <alzurasep@gmail.com>
2025-03-09 22:51:48 +0300

    Sp command 13

--- src/func/spotifyGet.py
+++ src/func/spotifyGet.py
@@ -10,7 +10,7 @@ def getToken():
 
     head = { 'Content-Type': "application/x-www-form-urlencoded" }
 
-    tokenGet = requests.post( "https://accounts.spotify.com/api/token", data = f"grant_type=client_credentials&client_id={ SpotifyClientID }&client_secret={ SpotifyClientSECRET }", headers = head )
+    tokenGet = requests.post( "https://accounts.spotify.com/api/token", data = f"grant_type=client_credentials&client_id=a261ed3bb88942a0b08d130a40920462&client_secret=f4800157163347518ad94a2e017eb550", headers = head )
     
     jsonTOKEN = tokenGet.json()
     accessTOKEN = jsonTOKEN[ 'access_token' ]
@@ -54,7 +54,6 @@ def getPlaylists(hesap):
 def searchAlbum(album):
     API = requests.get( f"https://api.spotify.com/v1/search?q={album}&type=album", headers = AUTH )
     APIjson = API.json()
-    APIalbum = APIjson['tracks']['items'][0]
-    returnedValue = APIalbum['album']['external_urls']['spotify']
+    APIalbum = APIjson['albums']['items'][0]
+    returnedValue = APIalbum['external_urls']['spotify']
     return returnedValue
-
